d083470a9432435d6ce99cbc6ccd9afba2d134c3
Alexandre Taillet <altata01@hotmail.com>
2025-05-04 21:48:15 +0200

    add: pipe inside pas_client

--- pas_client.c
+++ pas_client.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PAS_CMAN_IPL_PATH "./target/release/pas-cman-ipl"
 int main(int argc, char *argv[]) {
   struct GameState state;
   if (argv == NULL || argc != 3) {
@@ -13,6 +14,29 @@ int main(int argc, char *argv[]) {
   char *host = argv[1];
   int port = atoi(argv[2]);
 
+  // Exec GUI in ./target/release/pas-cman-ipl
+  int pipefd[2];
+  int ret = spipe(pipefd);
+
+  int childId = sfork();
+
+  if (childId != 0) {
+    // Exec the GUI and replace this proc by it
+    // redirect
+    FileDescriptor sysin = 0;
+    // Redirect stdin to the read end of the pipe
+    sdup2(pipefd[0], sysin);
+    ret = sclose(pipefd[1]);
+    sexecl(PAS_CMAN_IPL_PATH, PAS_CMAN_IPL_PATH, NULL);
+    perror("Failed to exec pas-cman-ipl");
+    exit(EXIT_FAILURE);
+  }
+
+  // Child is connecting to the socket
+  // close the read pipe, because we just need to send to the GUI.
+  ret = sclose(pipefd[0]);
+
+  // Client Socket init
   int sockfd = ssocket();
 
   sconnect(host, port, sockfd);
@@ -26,7 +50,7 @@ int main(int argc, char *argv[]) {
   char buffer[1024];
   ssize_t n;
   while ((n = sread(sockfd, buffer, sizeof(buffer))) > 0) {
-    swrite(sout, buffer, n);
+    swrite(pipefd[1], buffer, n);
   }
 
   return EXIT_SUCCESS;
